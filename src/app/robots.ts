a06bacadb7cb038846583aaa5ff97436e7d409de
Alexandre Chambu <amanichambu48@gmail.com>
2023-12-18 12:44:38 +0000

    add robots file

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,10 +1,16 @@
 import { MetadataRoute } from "next";
+import { languages } from "./i18n/settings";
 
 const robots: () => MetadataRoute.Robots = () => {
     return {
-        rules: [],
-        host: "",
-        sitemap: "",
+        rules: [
+            {
+                userAgent: "*",
+                disallow: ["/api"],
+                allow: languages.map((lang) => `/${lang}`),
+            },
+        ],
+        sitemap: "https://gorilla-fund.vercel.app",
     };
 };
 
